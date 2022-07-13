f85a387fae118186a8cc960cb290b62a8bb3abc1
Melvin <65771068+melvinb14@users.noreply.github.com>
2022-07-13 13:04:25 +0200

    Added JEI support

---
+++ src/main/java/com/melvinbur/archbows/core/intergration/JEIArchBowsPlugin.java
@@ -0,0 +1,15 @@
+package com.melvinbur.archbows.core.intergration;
+
+import com.melvinbur.archbows.ArchBows;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import net.minecraft.resources.ResourceLocation;
+
+
+@JeiPlugin
+public class JEIArchBowsPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(ArchBows.MOD_ID, "jei_plugin");
+    }
+}
